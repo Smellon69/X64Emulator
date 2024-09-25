a5e7db95fe923f660ae4f90eac9326e8bc0d9feb
Smellon <74884194+Smellon69@users.noreply.github.com>
2024-09-25 10:01:58 -0400

    true x64!!! wow, revolutionary!

--- X64Emulator/x64emulator.cpp
+++ X64Emulator/x64emulator.cpp
@@ -20,7 +20,7 @@ private:
     uint64_t r8, r9, r10, r11, r12, r13, r14, r15;
 
     // Memory (simplified as a vector of bytes)
-    std::vector<uint8_t> memory;
+    std::vector<uint64_t> memory;
 
     // Stack (simplified)
     std::vector<uint64_t> stack;
